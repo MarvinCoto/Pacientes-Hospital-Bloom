110d050f936f73426f324c856bdc86901ed42723
Levi Saravia <20230170@ricaldone.edu.sv>
2024-07-19 23:30:24 -0600

    Creando de nuevo la clase conexion porque no sé porque se eliminó al traer los cambios de la rama master a la rama Levi

---
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -0,0 +1,22 @@
+package modelo
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+class ClaseConexion {
+
+    fun cadenaConexion(): Connection? {
+
+        try {
+            val url = "jdbc: oracle: thin: @10.10.0.53:1521:xe"
+            val usuario = ""
+            val contrasena = ""
+
+            val connection = DriverManager.getConnection(url, usuario, contrasena)
+            return connection
+        }catch (e: Exception){
+            println("error: $e")
+            return null
+        }
+    }
+}
