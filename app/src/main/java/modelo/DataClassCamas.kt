38bfa61b5e9c5ac343010851359b1758d1f7fa40
Marvin Coto <20230644@ricaldone.edu.sv>
2024-07-26 03:23:21 -0600

    Agrugué los spinners junto a otros cambios

---
+++ app/src/main/java/modelo/DataClassCamas.kt
@@ -0,0 +1,6 @@
+package modelo
+
+data class DataClassCamas(
+    var UUIDCama: String,
+    var N_Cama: String,
+)
---
+++ app/src/main/java/modelo/DataClassHabitaciones.kt
@@ -0,0 +1,6 @@
+package modelo
+
+data class DataClassHabitaciones(
+    var UUIDHabitacion: String,
+    var N_Habitacion: String,
+)
